8f5fc49fc69700156ed5afa9f9ce9648d6d1554c
arenn0 <34347338+arenn0@users.noreply.github.com>
2018-12-01 19:25:58 -0600

    Structure and Preprocessing
    Reorganized the structure of the project.
    File has been preprocessed, column 5 is for non-processed tweets, column 26 for processed tweets.

--- Actionability/main.py
+++ Actionability/main.py
@@ -1,17 +1,32 @@
-
-
 import csv
 import sys
-def loadDataset (dirName)
-with open(sys.argv[1]) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            print('Column names are {", ".join(row)}')
-            line_count += 1
-        else:
-            print('\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
-            line_count += 1
-    print('Processed {line_count} lines.')
+import os
 
+def load_dataset (dirName):
+    directory_path = dirName
+    directory_list = sorted(os.listdir(directory_path))
+    global n_docs
+    n_docs = len(directory_list)  # Size of the dataset
+    # print(n_docs)
+    corpus = {}
+    for i in range(n_docs):
+        # print(directory_list[i])  # Prints the names of the Datasets
+        corpus[directory_list[i]] = []
+        with open(directory_path + directory_list[i], encoding="utf8") as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            line_count = 0
+            for row in csv_reader:
+                if line_count == 0:
+                    print("Hello")
+                    # print(f'Column names are {", ".join(row)}')
+                else:
+                    corpus[directory_list[i]].append(row[5])
+
+                line_count += 1
+            print(f'Processed {line_count} lines.')
+    return corpus
+
+
+corpus = load_dataset(sys.argv[1])
+
+print(corpus)
--- Actionability/wnet.py
+++ Actionability/wnet.py
@@ -1,3 +1,3 @@
-from nltk.corpus import wordnet
+import nltk
 
-https://www.geeksforgeeks.org/get-synonymsantonyms-nltk-wordnet-python/
+from nltk.corpus import wordnet
