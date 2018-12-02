ebc4dba3a48d0a54132c08ecc211ff0d7e22c3d5
arenn0 <34347338+arenn0@users.noreply.github.com>
2018-12-01 19:50:57 -0600

    c2

--- Actionability/wnet.py
+++ Actionability/wnet.py
@@ -1,23 +1,3 @@
-
 import nltk
-dler = nltk.downloader.Downloader()
-dler._update_index()
-dler.download("wordnet")
-# nltk.download()
-# First, you're going to need to import wordnet:
-from nltk.corpus import wordnet as wordnet
 
-# Then, we're going to use the term "program" to find synsets like so:
-syns = wordnet.synsets("program")
-
-# An example of a synset:
-print(syns[0].name())
-
-# Just the word:
-print(syns[0].lemmas()[0].name())
-
-# Definition of that first synset:
-print(syns[0].definition())
-
-# Examples of the word in use in sentences:
-print(syns[0].examples())
+from nltk.corpus import wordnet
