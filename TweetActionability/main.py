2b97e14dca227a90fffb00ee8e0e406610a6435d
arenn0 <34347338+arenn0@users.noreply.github.com>
2018-12-02 18:30:22 -0600

    TfIdf added

--- TweetActionability/main.py
+++ TweetActionability/main.py
@@ -1,13 +1,34 @@
 import csv
 import sys
 import os
-
+import string
+import nltk
+import tfidf
 
 def preprocess_corpus(corpus):
+    c = {}
+    exclude = set(string.punctuation)
+    exclude.add('.')
+    [exclude.add(i) for i in range(10)]
+    exclude.add('amp')
+    # exclude.add('#')
+
     for file_name in corpus:
+        c[file_name] = []
         for i in corpus[file_name]:
-            for word in corpus[file_name][i]:
-                pass
+            words = []
+            for word in i.split():
+                #print(word)
+                w = ''.join(ch for ch in word if ch not in exclude)
+                #print(w)
+                if 'http' in w or w in exclude:
+                    pass
+                else:
+                    words.append(w.lower())
+            c[file_name].append(words)
+
+    # print(c)
+    return c
 
 
 
@@ -26,17 +47,28 @@ def load_dataset (dirName):
             line_count = 0
             for row in csv_reader:
                 if line_count == 0:
-                    print("Hello")
-                    # print(f'Column names are {", ".join(row)}')
+                    # print("Hello")
+                    print(f'Column names are {", ".join(row)}')
                 else:
                     corpus[directory_list[i]].append(row[5])
 
                 line_count += 1
-            print(f'Processed {line_count} lines.')
+            #print(f'Processed {line_count} lines.')
     return corpus
 
 
 corpus = load_dataset(sys.argv[1])
 
-print(corpus)
+#print(corpus)
 corpus = preprocess_corpus(corpus)
+
+[dictionary, max_docs, n_tweets] = tfidf.create_dictionary(corpus)
+dictionary_text_tfidf = tfidf.compute_tfidf(dictionary, dictionary, max_docs, n_tweets)
+
+
+print(dictionary)
+print(dictionary_text_tfidf)
+
+filtered_final_queries = get_queries(associations)
+
+similarities = tfidf.calc_rank(dictionary_text_tfidf, dictionary_queries_tfidf, filtered_final_queries)
