d2cf9de2242f76080cdb742a0ef819cd11dc80f7
edosavini <edoardosavini95@gmail.com>
2018-12-12 23:29:52 -0600

    updated with max

--- TweetActionability/wnet.py
+++ TweetActionability/wnet.py
@@ -12,21 +12,21 @@ from nltk.corpus import wordnet as wordnet
 def expand_list(wordlist):
     s_map = {}
     for word in wordlist:
-        # Then, we're going to use the term "program" to find synsets like so:
-
+        #  for each word in the file analyze its synonims (a word may have itself between its syns)
         s_map[word]=1
         print(s_map)
         syns = wordnet.synsets(word)
         l= 'lost.a.01'
         print(syns)
         i=0
+        # map to contain word + similarity
         similarity = {}
-        #  find similarity of word with its synonims
-        # if there is only a synonim, add it directly with weight 1
+        #  find similarity between our word and its synonims
+        #  if there is only a synonim different from the word itself, add it directly with weight 1
         if len(syns) == 1:
             s_map[syns[0].name().split(".")[0]] = 1
         else:
-            #  take all the meanings of word and sum their similarity with all the other synonims
+            #  for all the meanings of word take the max similarity with all the other synonims
             for w in syns:
                 if w.name().startswith(word):
                     for synonims in syns:
@@ -34,32 +34,28 @@ def expand_list(wordlist):
                         if i == 0:
                             similarity[synonims.name()] = 0
                         if s is not None:
-                            similarity[synonims.name()] += s
-                        else:
-                            similarity[synonims.name()] += 0
+                            if s > similarity[synonims.name()]:
+                                similarity[synonims.name()] = s
+                            #  similarity[synonims.name()] += s
                         print(similarity)
                     i=i+1
                     print(i)
             sim_clean = {}
             rep = {}
-            #  compute the mean similarity
+            #  create new map to contain the max similarity
             for w in syns:
-                similarity[w.name()] /= i
                 if similarity[w.name()] != 0:
                     sim_clean[w.name().split(".")[0]] = 0
-                    rep[w.name().split(".")[0]] = 0
             i=0
-            # aggregate all the same words in a new s_map
+            # between all the same words take the max similarities
             for w in syns:
-                if similarity[w.name()] != 0:
-                    sim_clean[w.name().split(".")[0]] += similarity[w.name()]
-                    rep[w.name().split(".")[0]] += 1
+                if similarity[w.name()] != 0 and similarity[w.name()] > sim_clean[w.name().split(".")[0]]:
+                    sim_clean[w.name().split(".")[0]] = similarity[w.name()]
             print(sim_clean)
             print(rep)
-            # compute the mean of the words in the new s_map and add them to the final s_map
+            # add the new words + similarity value to the final s_map
             for k, v in sim_clean.items():
                 if k != word:
-                    v = v/rep[k]
                     s_map[k] = v
 
         print(s_map)
@@ -76,3 +72,5 @@ def read_file(file):   # "ManuallyAnnotatedWords.txt"
 
     print(s_list)
     expand_list(s_list)
+
+read_file("ManuallyAnnotatedWords.txt")
