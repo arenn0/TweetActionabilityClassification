03e06cc576858ca36a0feabd47ff0972de37eeb7
edosavini <edoardosavini95@gmail.com>
2018-12-14 11:56:29 -0600

    wnet without prints

--- TweetActionability/wnet.py
+++ TweetActionability/wnet.py
@@ -14,10 +14,9 @@ def expand_list(wordlist):
     for word in wordlist:
         #  for each word in the file analyze its synonims (a word may have itself between its syns)
         s_map[word]=1
-        print(s_map)
+        # print(s_map)
         syns = wordnet.synsets(word)
-        l= 'lost.a.01'
-        print(syns)
+        # print(syns)
         i=0
         # map to contain word + similarity
         similarity = {}
@@ -37,11 +36,10 @@ def expand_list(wordlist):
                             if s > similarity[synonims.name()]:
                                 similarity[synonims.name()] = s
                             #  similarity[synonims.name()] += s
-                        print(similarity)
+                        # print(similarity)
                     i=i+1
-                    print(i)
+                    # print(i)
             sim_clean = {}
-            rep = {}
             #  create new map to contain the max similarity
             if len(similarity) > 0:
                 for w in syns:
@@ -52,14 +50,13 @@ def expand_list(wordlist):
                 for w in syns:
                     if similarity[w.name()] != 0 and similarity[w.name()] > sim_clean[w.name().split(".")[0]]:
                         sim_clean[w.name().split(".")[0]] = similarity[w.name()]
-                print(sim_clean)
-                print(rep)
+                #print(sim_clean)
                 # add the new words + similarity value to the final s_map
                 for k, v in sim_clean.items():
                     if k != word:
                         s_map[k] = v
 
-        print(s_map)
+    print(s_map)
     return s_map
 
 
@@ -71,7 +68,7 @@ def read_file(file):   # "ManuallyAnnotatedWords.txt"
     for i in range(0, len(p)):
         s_list.append(p[i].strip("\n"));
 
-    print(s_list)
+    # print(s_list)
     return expand_list(s_list)
 
-# read_file("ManuallyAnnotatedWords.txt")
+read_file("ManuallyAnnotatedWords.txt")
